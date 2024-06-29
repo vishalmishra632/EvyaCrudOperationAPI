bb15df90b7c17d2bcb94a7de7b478ce0938440cf
vishal mishra <40206077+vishalmishra632@users.noreply.github.com>
2024-06-29 19:20:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 //};
 
 const corsOptions = {
-    origin: ['http://localhost:3001', 'https://your-frontend-url.vercel.app'],
+    origin: ['http://localhost:3001', 'https://evya-crud-operations.vercel.app'],
     optionsSuccessStatus: 200
 };
 app.use(cors(corsOptions));
